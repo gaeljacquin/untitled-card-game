bce9f24bea47717b314f5d80073f275baa6b7acc
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2024-12-22 13:57:08 -0500

    Flip through my titles on about page

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -3,8 +3,11 @@
 import AnimatedLogoDynamic from '@/components/animated-logo-dynamic';
 import { SectionCard } from '@/components/section-card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { FlipWords } from '@/components/ui/flip-words';
 
 export default function About() {
+  const myTitles = ['Developer', 'Designer'];
+
   return (
     <div className="max-w-4xl mx-auto space-y-8 mt-16">
       <SectionCard title="About" className="text-center text-white p-4">
@@ -29,7 +32,9 @@ export default function About() {
                 <AvatarFallback>Profile Picture</AvatarFallback>
               </Avatar>
               <div>
-                <h4 className="font-semibold">Game Design</h4>
+                <h4 className="font-semibold">
+                  <FlipWords words={myTitles} className="text-white" />
+                </h4>
                 <p>{process.env.author}</p>
               </div>
             </div>
@@ -40,7 +45,7 @@ export default function About() {
                 <AvatarFallback>Fireworks</AvatarFallback>
               </Avatar>
               <div>
-                <h4 className="font-semibold">Playtesting</h4>
+                <h4 className="font-semibold">Playtesters</h4>
                 <p>Friends & Family</p>
               </div>
             </div>
