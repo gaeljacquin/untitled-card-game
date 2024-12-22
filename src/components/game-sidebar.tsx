11b7aeebb9eae0cd230a19f58c9d09fb487d1930
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2024-12-21 22:26:24 -0500

    Remove container class from play page
    Made minor adjustments to sidebar

--- src/components/game-sidebar.tsx
+++ src/components/game-sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { CheckCircle, ChevronsDown, ChevronsUp, X } from 'lucide-react';
+import { CheckCircle, ChevronsDown, ChevronsUp, CircleX } from 'lucide-react';
 import AnimatedLogoDynamic from '@/components/animated-logo-dynamic';
 import { SectionCard } from '@/components/section-card';
 import { TerminalDisplay } from '@/components/terminal-display';
@@ -62,85 +62,95 @@ export function GameSidebar() {
 
           <Separator />
 
-          <div className="space-y-4">
-            <div className="space-y-2">
-              <h3 className="text-lg font-semibold">Stats</h3>
-              <div className="flex items-center justify-between gap-4">
-                <div className="flex flex-col items-center justify-center w-full">
-                  <span className="text-sm">Current Score</span>
-                  <TerminalDisplay
-                    value={currentScore}
-                    noBueno={currentScore < 0}
-                    className="flex w-full"
-                  />
-                </div>
-                <div className="flex flex-col items-center justify-center w-full">
-                  <span className="text-sm">Discards</span>
-                  <TerminalDisplay
-                    value={discards}
-                    noBueno={discards > 0}
-                    className="flex w-full"
-                  />
-                </div>
+          <div className="space-y-2">
+            <h3 className="text-lg font-semibold">Actions</h3>
+            <div className="grid grid-cols-2 gap-4">
+              <Button variant="default">Claim</Button>
+              <Button variant="default">Button 1</Button>
+              <Button variant="destructive">Discard</Button>
+              <Button variant="outline" className="text-black">
+                Button 2
+              </Button>
+            </div>
+          </div>
+
+          <Separator />
+
+          <div className="space-y-2">
+            <h3 className="text-lg font-semibold">Stats</h3>
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex flex-col items-center justify-center w-full">
+                <span className="text-sm">Current Score</span>
+                <TerminalDisplay
+                  value={currentScore}
+                  noBueno={currentScore < 0}
+                  className="flex w-full"
+                />
               </div>
-              <div className="flex items-center justify-between gap-4">
-                <div className="flex flex-col items-center justify-center w-full">
-                  <span className="text-sm">Valid Words</span>
-                  <TerminalDisplay value={validWords} className="flex w-full" />
-                </div>
-                <div className="flex flex-col items-center justify-center w-full">
-                  <span className="text-sm">Invalid Words</span>
-                  <TerminalDisplay
-                    value={invalidWords}
-                    noBueno={invalidWords > 0}
-                    className="flex w-full"
-                  />
-                </div>
+              <div className="flex flex-col items-center justify-center w-full">
+                <span className="text-sm">Discards</span>
+                <TerminalDisplay value={discards} noBueno={discards > 0} className="flex w-full" />
               </div>
-              <div className="flex items-center justify-between gap-4">
-                <div className="flex flex-col items-center justify-center w-full">
-                  <span className="text-sm">Total Score</span>
-                  <TerminalDisplay
-                    value={totalScore}
-                    noBueno={totalScore < 0}
-                    className="flex w-full"
-                  />
-                </div>
+            </div>
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex flex-col items-center justify-center w-full">
+                <span className="text-sm">Valid Words</span>
+                <TerminalDisplay value={validWords} className="flex w-full" />
               </div>
-              <>
-                <motion.header initial={false} onClick={() => setCollapsibleOpen(!collapsibleOpen)}>
-                  <div className="flex items-center justify-center space-x-4 mt-4">
-                    <div
-                      className="flex items-center justify-center space-x-2 border border-gray-200 rounded-lg px-2 w-full"
-                      role="button"
-                    >
-                      <p className="text-sm font-light w-full">
-                        {collapsibleOpen ? 'Hide' : 'Show More'}
-                      </p>
-                      <Button variant="ghost" size="sm" className="w-9 p-0">
-                        {collapsibleOpen ? (
-                          <ChevronsUp className="h-4 w-4" />
-                        ) : (
-                          <ChevronsDown className="h-4 w-4" />
-                        )}
-                        <span className="sr-only">Toggle</span>
-                      </Button>
-                    </div>
+              <div className="flex flex-col items-center justify-center w-full">
+                <span className="text-sm">Invalid Words</span>
+                <TerminalDisplay
+                  value={invalidWords}
+                  noBueno={invalidWords > 0}
+                  className="flex w-full"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex flex-col items-center justify-center w-full">
+                <span className="text-sm">Total Score</span>
+                <TerminalDisplay
+                  value={totalScore}
+                  noBueno={totalScore < 0}
+                  className="flex w-full"
+                />
+              </div>
+            </div>
+            <>
+              <motion.header initial={false} onClick={() => setCollapsibleOpen(!collapsibleOpen)}>
+                <div className="flex items-center justify-center space-x-4 mt-4">
+                  <div
+                    className="flex items-center justify-center space-x-2 border border-gray-200 rounded-lg px-2 w-full"
+                    role="button"
+                  >
+                    <p className="text-sm font-light w-full">
+                      {collapsibleOpen ? 'Hide' : 'Show More'}
+                    </p>
+                    <Button variant="ghost" size="sm" className="w-9 p-0">
+                      {collapsibleOpen ? (
+                        <ChevronsUp className="h-4 w-4" />
+                      ) : (
+                        <ChevronsDown className="h-4 w-4" />
+                      )}
+                      <span className="sr-only">Toggle</span>
+                    </Button>
                   </div>
-                </motion.header>
-                <AnimatePresence initial={false}>
-                  {collapsibleOpen && (
-                    <motion.section
-                      key="content"
-                      initial="collapsed"
-                      animate="open"
-                      exit="collapsed"
-                      variants={{
-                        open: { opacity: 1, height: 'auto' },
-                        collapsed: { opacity: 0, height: 0 },
-                      }}
-                      transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
-                    >
+                </div>
+              </motion.header>
+              <AnimatePresence initial={false}>
+                {collapsibleOpen && (
+                  <motion.section
+                    key="content"
+                    initial="collapsed"
+                    animate="open"
+                    exit="collapsed"
+                    variants={{
+                      open: { opacity: 1, height: 'auto' },
+                      collapsed: { opacity: 0, height: 0 },
+                    }}
+                    transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+                  >
+                    <div className="space-y-2">
                       <div className="flex items-center justify-between gap-4">
                         <div className="flex flex-col items-center justify-center w-full">
                           <span className="text-sm">Max Streak</span>
@@ -157,16 +167,16 @@ export function GameSidebar() {
                           <TerminalDisplay value={highScore} className="flex w-full" />
                         </div>
                       </div>
-                    </motion.section>
-                  )}
-                </AnimatePresence>
-              </>
-            </div>
+                    </div>
+                  </motion.section>
+                )}
+              </AnimatePresence>
+            </>
           </div>
 
           <Separator />
 
-          <div className="space-y-4">
+          <div className="space-y-2">
             <h3 className="text-lg font-semibold">Words Found</h3>
             <Command className="bg-black/50 border-white/20">
               <CommandList className="w-full mt-2 h-36">
@@ -179,27 +189,15 @@ export function GameSidebar() {
                         value={item.word}
                       >
                         <span>{item.word.toLocaleUpperCase()}</span>
-                        <span>{item.valid ? <CheckCircle /> : <X />}</span>
+                        <span>
+                          {item.valid ? <CheckCircle /> : <CircleX className="text-red-500" />}
+                        </span>
                       </CommandItem>
                     );
                   })}
                 </CommandGroup>
               </CommandList>
             </Command>
-          </div>
-
-          <Separator />
-
-          <div className="space-y-4">
-            <h3 className="text-lg font-semibold">Actions</h3>
-            <div className="grid grid-cols-2 gap-2">
-              <Button variant="default">Claim</Button>
-              <Button variant="default">Button 1</Button>
-              <Button variant="destructive">Discard</Button>
-              <Button variant="outline" className="text-black">
-                Button 2
-              </Button>
-            </div>
           </div>
         </div>
       </SectionCard>
