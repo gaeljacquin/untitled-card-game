139bc6cb0dca673fc2713a0da8d4f7c8028ebe9d
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2025-01-12 22:16:14 -0500

    Restore dictionary microservice
    For posterity

---
+++ dictionary/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+from lambda_function import ab_check
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "Hello, World"
+
+@app.route("/abcheck", methods=['POST'])
+def ab_check_rest():
+  word_map = request.json
+  res = ab_check(word_map)
+
+  return jsonify(res), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
