8a2892349f5082e57acd5792eae0ce5a02c34abb
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2025-04-04 00:15:09 -0400

    Use alias in import path
    Removed root dockerignore
    Pushing to main again to trigger workflow run

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GameGateway } from './game/game.gateway';
-import { GameService } from './game/game.service';
-import { GameModule } from './game/game.module';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { GameGateway } from '@/game/game.gateway';
+import { GameService } from '@/game/game.service';
+import { GameModule } from '@/game/game.module';
 
 @Module({
   imports: [GameModule],
