b509c42f5d0952c6e79501bfe22bcf75b3a18ea1
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2025-09-25 13:18:20 -0400

    chore: remove nestjs

--- apps/api/src/game/game.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { GameService } from '@/game/game.service';
-
-@Controller('game')
-export class GameController {
-  constructor(private readonly gameService: GameService) {}
-}
